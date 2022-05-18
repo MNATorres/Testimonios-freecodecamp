549df4b3f531f3537f2dab3f3abde564772fca6a
Matias <mtri21019@gmail.com>
2022-05-18 19:36:26 -0300

    se agregan bolds

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Testimonio from './componentes/Testimonio';
 
@@ -7,29 +6,29 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-          <Testimonio 
+        <Testimonio
           nombre='Shawn Wang'
           pais='Singapur'
           imagen='shawn'
           cargo='Ingeniero de Software'
           empresa='Amazon'
-          testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.' />
-          <Testimonio 
+          testimonio={<p>"Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras <b>como ingeniero de software</b>. freeCodeCamp cambió mi vida."</p>} />
+        <Testimonio
           nombre='Sarah Chima'
           pais='Nigeria'
           imagen='sarah'
           cargo='Ingeniera de Software'
           empresa='ChatDesk'
-          testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.' />
-          <Testimonio 
+          testimonio={<p>freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación <b>de un nivel de principiante total a un nivel muy seguro.</b> Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.</p>} />
+        <Testimonio
           nombre='Emma Bostian'
           pais='Suecia'
           imagen='emma'
           cargo='Ingeniera de Software'
           empresa='Spotify'
-          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.' />
-            
-        </div>
+          testimonio={<p>Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras <b>de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que </b>necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'</p>} />
+
+      </div>
     </div>
   );
 }
